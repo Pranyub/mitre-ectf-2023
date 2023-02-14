a9993e65d6a6aaa252c717bfff48c9c51232c3ec
Pranav Gopalkrishnan <36649761+Metafern@users.noreply.github.com>
2023-02-14 11:29:36 -0800

    add car libs

--- fob/src/authentication.c
+++ fob/src/authentication.c
@@ -21,9 +21,17 @@ void init_random(void) {
     uint8_t seed[SEED_SIZE];
     eeprom_read(seed, SEED_SIZE, EEPROM_RAND_ADDR);
     uint64_t e_factory = FACTORY_ENTROPY;
-    
     br_hmac_drbg_init(&ctx, &br_sha256_vtable, &e_factory, 8);
     br_hmac_drbg_update(&ctx, seed, SEED_SIZE);
+
+    br_sha256_context sha_ctx;
+    br_sha256_init(&sha_ctx);
+    br_sha256_update(&sha_ctx, entropy_sram, 2048);
+    uint8_t hash_out[32];
+    br_sha256_out(&sha_ctx, hash_out);
+
+    br_hmac_drbg_update(&ctx, hash_out, 32);
+
 }
 
 void get_rand_bytes(uint8_t* out, size_t len) {
