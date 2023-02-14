26b2dbef3afeab575ab349a43e921aa32673abc9
Pranav Gopalkrishnan <36649761+Metafern@users.noreply.github.com>
2023-02-13 22:02:15 -0800

    add comments to init entropy

--- fob/src/authentication.c
+++ fob/src/authentication.c
@@ -3,13 +3,16 @@
 
 
 void issue_challenge(uint8_t* challenge) {
-    
+
 }
 
 void solve_challenge(uint8_t* challenge, uint8_t* response) {
 
 }
 
+// Initialize BearSSL's pseudorandom number generator. This function must be called on boot.
+//TODO: Define source of entropy and give it a seed
+//Updates static pool with factory secret and random entropy
 void init_random(void) {
     br_hmac_drbg_init(&ctx, &br_sha256_vtable, NULL, 0);
 }
