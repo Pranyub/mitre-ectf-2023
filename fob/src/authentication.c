7524f73c14e3b1da9e11ffb3c2b777b0d59f6a73
Pranav Gopalkrishnan <36649761+Metafern@users.noreply.github.com>
2023-02-22 15:44:22 -0800

    update challenge solution to use nonces

--- fob/src/authentication.c
+++ fob/src/authentication.c
@@ -163,6 +163,8 @@ void gen_solution(void) {
     br_sha256_update(&ctx_sha2, challenge, sizeof(challenge));
     br_sha256_update(&ctx_sha2, challenge_resp, sizeof(challenge_resp));
     br_sha256_update(&ctx_sha2, car_secret, sizeof(car_secret));
+    br_sha256_update(&ctx_sha2, c_nonce, sizeof(c_nonce));
+     br_sha256_update(&ctx_sha2, s_nonce, sizeof(s_nonce));
     br_sha256_out(&ctx_sha2, p.response);
 
     p.command_magic = UNLOCK_MGK;    
