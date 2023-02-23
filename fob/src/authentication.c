a0f9892b13c45bf4b3081c59b827759e1833babf
Pranav Gopalkrishnan <36649761+Metafern@users.noreply.github.com>
2023-02-23 10:21:37 -0800

    add doxygen comments

--- fob/src/authentication.c
+++ fob/src/authentication.c
@@ -9,53 +9,65 @@
 #include "uart.h"
 
 
-//initialize message header values
-void init_message(Message* out) {
+/**
+ * @brief Zeroes a message and sets the nonces
+ * 
+ * @param message the message to be initialized
+ */
+void init_message(Message* message) {
     safe_memset(&current_msg, 0, sizeof(current_msg));
-    out->c_nonce = c_nonce;
-    out->s_nonce = s_nonce;
+    message->c_nonce = c_nonce;
+    message->s_nonce = s_nonce;
 }
 
 
-/* verify that a message is valid
-
-Requirements:
-    - client nonce must match stored client nonce
-    - server nonce must match stored server nonce (unless no server nonce is present, in which case set it)
-    - the recieved packet type matches the next expected packet type
-    - the payload size is nonzero
-    - the payload size matches the expected size for a specific payload type (TODO; maybe implement elsewhere?)
-    - the hash of the payload matches the payload
-
-    - if any of these checks are failed, returns false. Otherwise return true.
-*/
+/**
+ * @brief Verifies that a given message is authenticated and valid.
+ * 
+ * @return true if all of the following conditions are met:
+ *  - message target matches device type
+ *  - client and server nonces are valid
+ *  - the message type is expected
+ *  - the hmac of the message is valid
+ * @return false otherwise
+ * 
+ * @param message the message to be verified
+ */
 bool verify_message(Message* message) {
 
+    // Ensure message is going to the correct device type
     if(message->target != DEVICE_TYPE) {
         return false;
     }
 
+    // Ensure the client nonce is valid if it needs to be
     if(message->msg_magic != HELLO && message->c_nonce != c_nonce) {
         return false;
     }
     
+    // Ensure the server nonce is valid if it needs to be
     if (message->msg_magic != HELLO && message->msg_magic != CHALL && message->s_nonce != s_nonce)
     {
        return false;
     }
 
+    // Redundant(?) check to ensure there is a payload
     if(message->payload_size < 1) {
         return false;
     }
 
+    // Ensure that the recieved packet type is the expected type of packet to be recieved
     if(next_packet_type != message->msg_magic || next_packet_type == 0) {
         return false;
     }
 
+    // Ensure that the hmac of the packet is valid
     uint8_t hash[32];
     br_hmac_context ctx_hmac;
     br_hmac_init(&ctx_hmac, &ctx_hmac_key, sizeof(hash));
-    br_hmac_update(&ctx_hmac, &message, sizeof(Message) - 512 + message->payload_size - sizeof(hash));
+    br_hmac_update(&ctx_hmac, &message, sizeof(Message) - PAYLOAD_BUF_SIZE \
+        + message->payload_size - sizeof(hash));
+
     br_hmac_outCT(&ctx_hmac, NULL, 0, 0, 0, hash);
 
     if(!timingsafe_memcmp(hash, message->payload_hash, sizeof(hash))) {
@@ -65,29 +77,42 @@ bool verify_message(Message* message) {
     return true;
 }
 
-// Adds a given message to a payload and computes the corresponding hash
-
+/**
+ * @brief Finalizes the addition of a payload to a message by signing it and adding the hash & size fields
+ * 
+ * @param message the message to be signed
+ * @param size the size of the message's payload
+ */
 void message_sign_payload(Message* message, size_t size) {
+
+    //make sure the size isn't too large
     if(size > PAYLOAD_BUF_SIZE) {
         return;
     }
 
+    //set the size
     message->payload_size = size;
+
     br_hmac_context ctx_hmac;
     br_hmac_init(&ctx_hmac, &ctx_hmac_key, sizeof(message->payload_hash));
-    br_hmac_update(&ctx_hmac, &message, sizeof(Message) - PAYLOAD_BUF_SIZE + message->payload_size - sizeof(message->payload_hash));
+    br_hmac_update(&ctx_hmac, &message, sizeof(Message) - PAYLOAD_BUF_SIZE \
+        + message->payload_size - sizeof(message->payload_hash));
+
     br_hmac_outCT(&ctx_hmac, NULL, 0, 0, 0, message->payload_hash);
 }
 
-
-void start_unlock_sequence(void) {
-    reset_state();
-    gen_hello();
-    uart_send_message(DEVICE_UART, &current_msg);
-    next_packet_type = CHALL;
-}
-
-
+/**
+ * @brief Attempts to read a message packet from UART and parses it
+ * 
+ * This is where the core Conversation logic occurs - the function
+ * verifies that the incoming message is authenticated and valid and
+ * parses the data accordingly.
+ * 
+ * It then attempts to craft a response to the incoming packet
+ * 
+ * If the incoming packet is invalid, the internal state of the current conversation
+ * is reset.
+ */
 void parse_inc_message(void) {
     uart_read_message(DEVICE_UART, &current_msg);
     //remove second verification if slow
@@ -104,10 +129,11 @@ void parse_inc_message(void) {
     case CHALL:
         if(handle_chall(&current_msg)) {
             gen_solution();
+            break;
         }
-        break;
     case END:
         handle_end(&current_msg);
+        reset_state();
         break;
     #endif
 
@@ -115,15 +141,16 @@ void parse_inc_message(void) {
     case HELLO:
         if(handle_hello(&current_msg)) {
             gen_chall();
+            break;
         }
-        break;
     case SOLVE:
         if(handle_solution(&current_msg)) {
             gen_end();
+            break;
         }
     #endif
     default:
-        uart_send_raw(HOST_UART, "bad magic\n", 11);
+        uart_send_raw(HOST_UART, "bad packet\n", 11);
         reset_state();
         break;
     }
@@ -137,13 +164,34 @@ void send_next_message(void) {
 }
 
 
+/************************************************************************************
+ * The following section of functions is reserved only for use with a fob fimrware. *
+ * A car does not require these functions.                                          *
+ ************************************************************************************/
+
 #ifdef FOB_TARGET
+
+/**
+ * @brief Starts a conversation with a car to unlock the fob
+ * 
+ * This function should be called when the unlock button is pressed on a fob.
+ * It resets the internal state of the current active conversation and
+ * sends a new hello message through the device UART.
+ */
+void start_unlock_sequence(void) {
+    reset_state();
+    gen_hello();
+    uart_send_message(DEVICE_UART, &current_msg);
+    next_packet_type = CHALL;
+}
+
 /* Creates and sends a hello message as part of the Conversation Protocol
 
 The hello message is the first stage of the Conversation Protocol.
 It consists of a 32 byte random value that will later be used in the challenge solution
 
-As of now, the creation of the packet and the sending of the packet occur in one function. Should this change?
+As of now, the creation of the packet and the sending of the packet occur in one function.
+Should this change?
 */
 void gen_hello(void) {
     safe_memset(&current_msg, 0, sizeof(Message));
@@ -223,6 +271,12 @@ bool handle_end(Message* message) {
     return true;
 }
 #endif
+
+
+/************************************************************************************
+ * The following section of functions is reserved only for use with a car fimrware. *
+ * A fob (paired or unpaired) does not require these functions.                     *
+ ************************************************************************************/
 
 #ifdef CAR_TARGET
 
@@ -312,26 +366,39 @@ void reset_state(void) {
 }
 
 
-// Initialize BearSSL's pseudorandom number generator. This function must be called on boot.
-//TODO: Define source of entropy and give it a seed
-//Updates static pool with factory secret and random entropy
+/**
+ * Function that initializes the pseudorandom generator with entropy from the following sources:
+ *  - factory seed
+ *  - persistent mutating code stored in eeprom
+ *  - random bits from uninitialized section of ram
+ *  - temperature (TODO)
+ * 
+ * Upon initialization, it overrides the stored seed in eeprom with a different value.
+ * Also initializes HMAC context
+ */
 void rand_init(void) {
 
     eeprom_init();
 
-    uint8_t seed[SEED_SIZE];
-    eeprom_read(seed, SEED_SIZE, EEPROM_RAND_ADDR);
+    // Update rand with factory seed
     uint64_t e_factory = FACTORY_ENTROPY;
     br_hmac_drbg_init(&ctx_rand, &br_sha256_vtable, &e_factory, 8);
+
+    // Update rand with EEPROM seed
+    uint8_t seed[SEED_SIZE];
+    eeprom_read(seed, SEED_SIZE, EEPROM_RAND_ADDR);
     br_hmac_drbg_update(&ctx_rand, seed, SEED_SIZE);
 
+    // Update rand with uninitialized ram
     br_sha256_context sha_ctx;
     br_sha256_init(&sha_ctx);
     br_sha256_update(&sha_ctx, (uint8_t*) RAND_UNINIT, 2048);
     uint8_t hash_out[32];
     br_sha256_out(&sha_ctx, hash_out);
-
     br_hmac_drbg_update(&ctx_rand, hash_out, 32);
+
+    //TODO: Update rand with temperature
+    //
 
     //override old source of persistent memory with new value
     br_hmac_drbg_generate(&ctx_rand, seed, SEED_SIZE);
