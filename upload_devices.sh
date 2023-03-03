99466e1ba7c31cf14f83931aebff08cbfe8cc70e
Pranav Gopalkrishnan <36649761+Metafern@users.noreply.github.com>
2023-03-03 15:34:50 -0800

    finish pin

--- upload_devices.sh
+++ upload_devices.sh
@@ -3,7 +3,7 @@ python3 -m ectf_tools device.load_hw --dev-in ./carout --dev-name carf1 --dev-se
 && fg
 
 
-
-
+#python3 -m ectf_tools device.load_hw --dev-in ./ufobout --dev-name unpairedfob --dev-serial /dev/cu.usbmodem0E2349AA1 \
+#python3 -m ectf_tools device.load_hw --dev-in ./carout --dev-name carf1 --dev-serial /dev/cu.usbmodem0E2349B71
 #python3 -m ectf_tools run.package --name stablev2 --deployment featuretest --package-out packageout --package-name pak1 --car-id 1 --feature-number 1
 #python3 -m ectf_tools run.enable --name stablev2 --package-in packageout --package-name pak1 --fob-bridge 1
